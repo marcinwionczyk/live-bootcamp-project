3f936f62f8a73d7d7f39b1b193cdd5357efe7745
Marcin Wionczyk <marcin.wionczyk@gmail.com>
2025-07-24 22:32:38 +0200

    added routes

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -14,7 +14,11 @@ impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
-            .route("/signup", post(signup));
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-token", post(verify_token))
+            .route("/verify-2fa", post(verify2fa));
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
@@ -29,5 +33,21 @@ impl Application {
 // Example route handler.
 // For now we will simply return a 200 (OK) status code.
 async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify2fa() -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
